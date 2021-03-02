9ee724f1e39da42219bb5a9a8bdfdfe1bda7974b
Jonathan Rovieri <jrovieri@gmail.com>
2021-03-02 14:55:18 -0300

    jpa and lombok added

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -1,7 +1,6 @@
 package guru.springframework.domain;
 
 import java.math.BigDecimal;
-
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -9,7 +8,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Ingredient {
 
@@ -19,51 +24,10 @@ public class Ingredient {
 
   private String description;
 
+  private BigDecimal amount;
+
   @OneToOne(fetch = FetchType.EAGER)
   private UnitOfMeasure uom;
 
-  private BigDecimal amount;
-
-  @ManyToOne
-  private Recipe recipe;
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public BigDecimal getAmount() {
-    return amount;
-  }
-
-  public void setAmount(BigDecimal amount) {
-    this.amount = amount;
-  }
-
-  public Recipe getRecipe() {
-    return recipe;
-  }
-
-  public void setRecipe(Recipe recipe) {
-    this.recipe = recipe;
-  }
-
-  public UnitOfMeasure getUom() {
-    return uom;
-  }
-
-  public void setUom(UnitOfMeasure uom) {
-    this.uom = uom;
-  }
+  @ManyToOne private Recipe recipe;
 }
--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,5 +1,6 @@
 package guru.springframework.domain;
 
+import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -14,7 +15,9 @@ import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import lombok.Data;
 
+@Data
 @Entity
 public class Recipe {
 
@@ -22,7 +25,7 @@ public class Recipe {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  private String description;
+  @Lob private String description;
 
   private Integer prepTime;
 
@@ -34,7 +37,7 @@ public class Recipe {
 
   private String url;
 
-  private String directions;
+  @Lob private String directions;
 
   @Lob private Byte[] image;
 
@@ -45,108 +48,23 @@ public class Recipe {
   private Notes notes;
 
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-  private Set<Ingredient> ingredients;
+  private Set<Ingredient> ingredients = new HashSet<>();
 
   @ManyToMany
   @JoinTable(
       name = "recipe_category",
       joinColumns = @JoinColumn(name = "recipe_id"),
       inverseJoinColumns = @JoinColumn(name = "category_id"))
-  private Set<Category> categories;
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public Integer getPrepTime() {
-    return prepTime;
-  }
-
-  public void setPrepTime(Integer prepTime) {
-    this.prepTime = prepTime;
-  }
-
-  public Integer getCookTime() {
-    return cookTime;
-  }
-
-  public void setCookTime(Integer cookTime) {
-    this.cookTime = cookTime;
-  }
-
-  public Integer getServings() {
-    return servings;
-  }
-
-  public void setServings(Integer servings) {
-    this.servings = servings;
-  }
-
-  public String getSource() {
-    return source;
-  }
-
-  public void setSource(String source) {
-    this.source = source;
-  }
-
-  public String getUrl() {
-    return url;
-  }
-
-  public void setUrl(String url) {
-    this.url = url;
-  }
-
-  public String getDirections() {
-    return directions;
-  }
-
-  public void setDirections(String directions) {
-    this.directions = directions;
-  }
-
-  public Byte[] getImage() {
-    return image;
-  }
-
-  public void setImage(Byte[] image) {
-    this.image = image;
-  }
-
-  public Notes getNotes() {
-    return notes;
-  }
+  private Set<Category> categories = new HashSet<>();
 
   public void setNotes(Notes notes) {
     this.notes = notes;
+    notes.setRecipe(this);
   }
 
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public Difficulty getDifficulty() {
-    return difficulty;
-  }
-
-  public void setDifficulty(Difficulty difficulty) {
-    this.difficulty = difficulty;
-  }
-
-  public Set<Ingredient> getIngredients() {
-    return ingredients;
-  }
-
-  public void setIngredients(Set<Ingredient> ingredients) {
-    this.ingredients = ingredients;
+  public Recipe addIngredient(Ingredient ingredient) {
+    this.ingredients.add(ingredient);
+    ingredient.setRecipe(this);
+    return this;
   }
 }
